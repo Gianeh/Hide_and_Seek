07f28e8be930a51c12355e2cc9eca0848c18ba37
gagliano-kevin <gagliano.kevin.97@gmail.com>
2024-02-06 14:56:49 +0100

    plot_script comments

--- plot_script.py
+++ plot_script.py
@@ -2,15 +2,20 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Script to plot the reward trend from the reward files relatively to each trained agent
 
-
-agents = ["alpha_0", "alpha_1", "alpha_2", "alpha_3", "hivemind_0"]
+# List of all the agents 
+agents = ["alpha_0", "alpha_1", "alpha_2", "alpha_3", "hivemind_0", "beta_0", "beta_1", "beta_2", "beta_3", "beta_4"]
 
 for a in range(len(agents)):
 
+    # Path of the reward file to read 
     reward_path = "./"+agents[a]+"/reward"
+
+    # Final path to save the plots
     plot_path = "./"+agents[a]+"/reward_plots"
 
+    # If the directory for the agent does not exist, continue
     if not os.path.exists("./"+agents[a]): continue
 
     if not os.path.exists(plot_path):
@@ -35,7 +40,7 @@ for a in range(len(agents)):
 
                 values = line.split(';')
 
-                # Numpy array for the data
+                # Numpy array for the data to plot
                 data = np.array(values[1:-1], dtype=float)
 
                 # Create a new figure
@@ -50,10 +55,13 @@ for a in range(len(agents)):
                 plt.xlabel('Generation')
                 plt.ylabel('Reward')
 
+                # Numpy array with a sequence of value between min(data) and max(data) spaced by step
                 y_ticks = np.arange(min(data), max(data)+1, step=20)
+                
+                # Setting tick marks on the y-axis 
                 plt.yticks(y_ticks)
 
-                # Save plot
+                # Save plot                                 # removing extension ".txt" 
                 image_name = os.path.join(plot_path, f'plot_{os.path.splitext(txt_file)[0]}.png')
                 plt.savefig(image_name)
 
